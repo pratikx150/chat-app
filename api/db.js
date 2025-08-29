788ab48665cf5e64b65b76a9c6b2ac78557f98ed
pratikx150 <pratikkumar11500@gmail.com>
2025-08-29 16:35:27 +0530

    Update db.js

--- api/db.js
+++ api/db.js
@@ -1,1 +1,9 @@
+const { Pool } = require('pg');
 
+const pool = new Pool({
+  connectionString: process.env.NEON_DB_URL,
+});
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+};
