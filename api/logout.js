53cbd75363154edd3f4bece060225b039c9c2bec
pratikx150 <pratikkumar11500@gmail.com>
2025-08-29 17:50:22 +0530

    Delete api/logout.js

--- api/logout.js
+++
@@ -1,8 +0,0 @@
-const { query } = require('./db');
-
-module.exports = async (req, res) => {
-  if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' });
-  const { username } = req.body;
-  await query('UPDATE users SET is_online = false WHERE username = $1', [username]);
-  res.json({ status: 'success' });
-};
